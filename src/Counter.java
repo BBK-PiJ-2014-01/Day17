2d8df6cc49549324d97ed6af838bdd81f6e77f85
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-02-03 00:47:38 +0000

    Code complete

---
+++ src/Counter.java
@@ -0,0 +1,12 @@
+/**
+ * Created by PierreM on 03/02/2015.
+ */
+public class Counter {
+    private int n = 0;
+    public void increase() {
+        n++;
+    }
+    public int getCount() {
+        return n;
+    }
+}
